63cdff8d1e379c7093aeb696a30c80ad79730dcf
Austin Stoudt <stoudaus000@gmail.com>
2015-03-04 10:36:43 -0800

    Update Blink.ino

--- arduino/Blink.ino
+++ arduino/Blink.ino
@@ -1,15 +1,16 @@
-
+//set pin to 13
 int led = 13;
-
+//set up code
 void setup() {                
-
+//output on pin 13
   pinMode(led, OUTPUT);     
 }
 
-
+//led starts on, waits a second, then turns off for a second.
 void loop() {
   digitalWrite(led, HIGH);   
   delay(1000);              
   digitalWrite(led, LOW);    
   delay(1000);              
 }
+//end code
